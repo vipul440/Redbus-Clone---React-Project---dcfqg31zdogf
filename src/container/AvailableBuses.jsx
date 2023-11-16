9e8f629a987a75f7f766a2b6f9035d15ac80098a
vipul440 <82093511+vipul440@users.noreply.github.com>
2023-11-16 17:40:01 +0530

    Update AvailableBuses.jsx

--- src/container/AvailableBuses.jsx
+++ src/container/AvailableBuses.jsx
@@ -1,3 +1,66 @@
+// import React, { useEffect, useState } from "react";
+// import SearchBus from "../Components/SearchBus.jsx";
+// import { useLocation, useParams, useNavigate } from "react-router-dom";
+// import PassengerDetailsModal from "../Components/PassengerDetailsModal.jsx";
+// import "./AvailableBuses.css";
+// import ShowTicket from "../Components/ShowTicket.jsx";
+
+// const AvailableBuses = ({ buses }) => {
+//   const location = useLocation();
+//   const [selectedBus, setSelectedBus] = useState(null);
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+//   const Navigate = useNavigate();
+  
+//   const handleBookTicket = (bus) => {
+//     setSelectedBus(bus);
+//     setIsModalOpen(true);
+//   };
+
+//   const handleCloseModal = () => {
+//     setSelectedBus(null);
+//     setIsModalOpen(false);
+//   };
+//   return (
+//     <div className="availableBuses">
+//       <h2 className="top">Available Buses</h2>
+//       <div className="contentBuses">
+
+//         <ul>
+//           {location?.state?.buses?.map((bus, index) => (
+//             <li key={index} className="busItem">
+//               <h3 className="busId">Bus ID: {bus.id}</h3>
+//               <p>Bus Name: {bus.busName}</p>
+//               <p>Ticket Price: {bus.ticketPrice}</p>
+//               <p>Date: {bus.date}</p>
+//               <p>Arrival Time: {bus.arrivalTime}</p>
+//               <p>Departure Time: {bus.departureTime}</p>
+//               <p>Source: {bus.source}</p>
+//               <p>Destination: {bus.destination}</p>
+//               <button onClick={() => handleBookTicket(bus)}>Book Ticket</button>
+//             </li>
+//           ))}
+//         </ul>
+
+//       </div>
+//       {isModalOpen && (
+//         <PassengerDetailsModal
+//           bus={selectedBus}
+//           onClose={handleCloseModal}
+//           onBookTicket={(passengerDetails) => {
+//             console.log("Passenger details:", passengerDetails);
+//             setSelectedBus(null);
+//             setIsModalOpen(false);
+//           }}
+//         />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default AvailableBuses;
+
+
+
 import React, { useEffect, useState } from "react";
 import SearchBus from "../Components/SearchBus.jsx";
 import { useLocation, useParams, useNavigate } from "react-router-dom";
@@ -9,8 +72,14 @@ const AvailableBuses = ({ buses }) => {
   const location = useLocation();
   const [selectedBus, setSelectedBus] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isBusAvailable, setIsBusAvailable] = useState(true);
   const Navigate = useNavigate();
-  
+
+  useEffect(() => {
+    const areBusesAvailable = location?.state?.buses?.length > 0;
+    setIsBusAvailable(areBusesAvailable);
+  }, [location]);
+
   const handleBookTicket = (bus) => {
     setSelectedBus(bus);
     setIsModalOpen(true);
@@ -22,9 +91,15 @@ const AvailableBuses = ({ buses }) => {
   };
   return (
     <div className="availableBuses">
-      <h2 className="top">Available Buses</h2>
+      {isBusAvailable ? (
+        <h2 className="top">Available Buses</h2>
+      ) : (
+        <h2 className="top">
+          Please check once , There is no Bus Available on this route or on this
+          Date
+        </h2>
+      )}
       <div className="contentBuses">
-
         <ul>
           {location?.state?.buses?.map((bus, index) => (
             <li key={index} className="busItem">
@@ -40,7 +115,6 @@ const AvailableBuses = ({ buses }) => {
             </li>
           ))}
         </ul>
-
       </div>
       {isModalOpen && (
         <PassengerDetailsModal
